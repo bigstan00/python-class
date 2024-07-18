e777bae16fecf9f56695150299e4607ea6564aef
bigstan00 <sdakwoji@gmail.com>
2024-07-18 12:53:30 +0100

    Python-class

---
+++ variables.py
@@ -0,0 +1,120 @@
+# This is for Variables in python
+
+#MyName = "Toothpick" #data type is string
+
+#Age = 2 # this is interger
+
+#Age = 2.5 # float data type
+
+#Istrue =  True ##this is the boolesn data type
+
+#List = [1 , 2 ,3 ,4 ,5] #list data type makes use of []
+
+#Tuple = (1 , 2 , 3 , 4 , 5) ## Tuples data makes use of ()
+
+#Dictionary + ["Name": "Toothpick" "Age": 2 "Istrue": True] ## collection of keyvalue pairs ("Name"key) "Toothpick is the value")
+
+#complex = 1 + 2
+
+
+#name = "Tobi"
+#age = 12
+#worth = 12.5
+#game = True
+#list_data = [1, 2, 3, 4, 5]
+#tuple_data = (1, 2, 3, 4, 5)
+#set_data = { 1, 2, 3, 4, 5}
+#dict_data  = {"Name": "Toothpick", "Age": 2}
+#data_complex = 13 + 12.5
+#print (type(name), type(age), type(worth), type(game), type(game), type(list_data), type(tuple_data), type(set_data), type(dict_data), type(data_complex), type(data_complex))
+
+#name = "Linux"
+
+#print (name[0])
+#print (name[1])
+#print (name[2])
+#print (name[3])
+#print (name[4])
+
+#Name = "James"
+#say = "Welcome to class, how old are you"
+#age = 10
+
+#print (Name, say, age)
+
+#name = "John"
+#message = """
+#Sam is a bad guy
+#kike is toothpick
+#Tobi is a chairman
+#"""
+
+#print (name , message)
+
+#list_name = ["sam", "loveth", "kike"]
+#list_name[0] = "Toothpick"
+#print (list_name)
+
+
+#num = 10
+
+#if num > 0:
+#    print("Number is positive")
+#elif num == 0:
+#    print("Number is zero")
+#else:
+#    print("Number is negative")
+
+# Example of for loop
+#fruits = ["apple", "banana", "cherry", "mango"]
+
+#for fruits in fruits:
+#    print(fruits)
+
+# Example of while loop
+#count = 6
+
+#while count < 10:
+##    print(count)
+ #   count += 1
+
+ # Example of break and continue
+#for num in range(10):
+##    if num == 5:
+#        break
+#    print(num)
+
+#for num in range(10):
+#    if num % 2 == 0:
+##        continue
+#   print(num)
+
+a = 5
+b = 5
+
+# Check if a is equal to b
+#if a == b:
+##    print("a is equal to b")
+#else:
+#    print("a is not equal to b")
+
+# Example 1: Breaking out of a for loop
+#numbers = [1, 2, 3, 4, 5, 6, 7]
+
+#for num in numbers:
+#    if num == 7:
+#        break  # Exit the loop when num is 4
+#    print(num)
+
+#print("Loop ended")
+
+#numbers = [1, 2, 3, 4, 5]
+
+#for num in numbers:
+#    if num % 2 == 0:
+#        continue  # Skip even numbers
+#    print(num)
+
+#print("Loop ended")
+
+x = int("3")      # x will be 3
